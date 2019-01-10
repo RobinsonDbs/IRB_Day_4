3ffb50ee702fb7529efd496bd41c0e9a019cb6b9
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-01-10 16:34:01 +0100

    exercice password

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,11 +1,45 @@
 require 'io/console'                              # lib pour cacher password
 
-def getpass                                       # demande un mot de passe sans l'afficher
-  prompt = "Donnes-moi ton mot de passe, stp > "
+  
+def signup                                       # demande un mot de passe sans l'afficher
+  prompt = "Définis ton mot de passe, stp > "
   print prompt
-  password = STDIN.noecho(&:gets).chomp           # mot de passe caché
+  $password = STDIN.noecho(&:gets).chomp           # mot de passe caché
+  puts ""
 end
 
-puts "puts\n" + getpass
 
-p "\n" + getpass
+# puts $password
+
+def login
+# demande un mot de passe sans l'afficher
+  prompt = "Entre ton mot de passe, stp > "
+  print prompt
+  $essai = STDIN.noecho(&:gets).chomp
+  while $essai != $password
+    prompt = "Recommence, stp > "
+    print prompt
+    $essai = STDIN.noecho(&:gets).chomp
+  end
+  accueil_nsa
+end
+
+def accueil_nsa
+
+  puts "\n\n\n"
+  puts " ██╔══██╗██║██╔════╝████╗  ██║██║   ██║██╔════╝████╗  ██║██║   ██║██╔════╝    ██╔══██╗    ██║     ██╔══██╗    ████╗  ██║██╔════╝██╔══██╗"
+  puts " ██████╗ ██╗███████╗███╗   ██╗██╗   ██╗███████╗███╗   ██╗██╗   ██╗███████╗     █████╗     ██╗      █████╗     ███╗   ██╗███████╗ █████╗ "
+  puts " ██████╔╝██║█████╗  ██╔██╗ ██║██║   ██║█████╗  ██╔██╗ ██║██║   ██║█████╗      ███████║    ██║     ███████║    ██╔██╗ ██║███████╗███████║"
+  puts " ██╔══██╗██║██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║   ██║██╔══╝      ██╔══██║    ██║     ██╔══██║    ██║╚██╗██║╚════██║██╔══██║"
+  puts " ██████╔╝██║███████╗██║ ╚████║ ╚████╔╝ ███████╗██║ ╚████║╚██████╔╝███████╗    ██║  ██║    ███████╗██║  ██║    ██║ ╚████║███████║██║  ██║"
+  puts " ╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝    ╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝"
+  puts "\n\n\n\n\n\n\nNous savons qui vous êtes !\n\nSincèrement,\nEdward Snowden\n\n\n"
+                                                                                                                                           
+  end
+
+def perform
+  signup
+  login
+end
+
+perform
