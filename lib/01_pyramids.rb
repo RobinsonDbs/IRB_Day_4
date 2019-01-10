1512d381965fec78a2ac42041445149e5e8280c3
RobinsonDbs <dubois.robinson@hotmail.fr>
2019-01-10 15:42:12 +0100

    ajout de pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -4,6 +4,8 @@
 #  #### 01111
 # ##### 11111
 
+#Half-Pyramid
+def half_pyramid
 puts "Bonjour, donne un chiffre entre 1 et 25 :"
 print "> "
 chiffre = gets.chomp.to_i
@@ -14,20 +16,72 @@ if chiffre < 1 || chiffre > 25
 else
     puts " ? OK. Voici ta pyramide :"
 end
-# Version simple
-puts "\n Version simple : \n\n"
+
 i = 1
 while i <= chiffre
     print " " * (chiffre - i)
     puts "#" * i
     i += 1
 end
+end
 
-puts "\n Version \"double\" : \n\n"
-i = 1
+half_pyramid
+
+#Full_Pyramid
+def full_pyramid
+    puts "Bonjour, donne un chiffre entre 1 et 25 :"
+    print "> "
+chiffre = gets.chomp.to_i
+    print chiffre
+
+
+if chiffre < 1 || chiffre > 25
+    puts " : Non ! Un chiffre entre 1 et 25 !"
+    exit
+else
+    puts " ? OK. Voici ta pyramide :"
+end
+    i = 1
 while i <= chiffre
     print " " * (chiffre - i)
-    puts "@" * i * 2
+    print "#" * i
+    puts "#" * (i-1)
+    i += 1
+end
+end
+
+full_pyramid
+
+#WTF_Pyramid
+def wtf_pyramid
+    puts "Bonjour, donne un chiffre impair entre 1 et 25 :"
+    print "> "
+chiffre = gets.chomp.to_i
+    print chiffre
+
+
+if chiffre < 1 || chiffre > 25 || chiffre.even?
+    puts " : Non ! Un chiffre impair entre 1 et 25 !"
+    exit
+
+else
+    puts " ? OK. Voici ta pyramide :"
+end
+    i = 1
+while i <= chiffre/2+1
+    print " " * (chiffre - i)
+    print "#" * i
+    puts "#" * (i-1)
     i += 1
 end
 
+until i == 0
+    print " " * (chiffre - i)
+    print "#" * i
+    puts "#" * (i-1)
+    i -= 1
+
+end
+end
+
+wtf_pyramid
