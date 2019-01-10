28e07a3e4887bdfa66425226365dbbd71b5ee91a
Olivier <olivier@ruechappe.fr>
2019-01-10 22:14:38 +0100

    modifié un peu les messages de jeu

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,40 +1,54 @@
-
+# lancé de dé aléatoire entre 1 et 6
 def lancer
-   $oie=rand(1..6) 
+    $oie=rand(1..6) 
 end
-
-
 
 $position = 0
+
+# tour de jeu
 def tour
-lancer
+    lancer
     case $oie 
     when 5, 6
-    puts "tu montes grâce à ton #{$oie}"
-    $position += 1
-    puts "tu es à la position #{$position}"
+        print "tu montes grâce à ton #{$oie}".chop
+        $position += 1
     when 1
-        if $position>1 
-    puts "tu descends à cause de ton #{$oie}"
-    $position -= 1
+        if $position > 1 
+            print "tu descends à cause de ton #{$oie}".chop
+            $position -= 1
         end
-    puts "tu es à la position #{$position}"
     else 
-    puts "tu ne bouges pas parce que tu as fait #{$oie}"
-    puts "tu es à la position #{$position}"
+        print "tu ne bouges pas parce que tu as fait #{$oie}".chop
     end
+    puts "(position #{$position})\n\n"
 end
 
+# jeu
 def jeu
     tour
     i =1
     while $position != 10
         tour
-    i +=1 
-    puts "tour #{i}"
+        i +=1 
+        puts "tour #{i}"
     end
-
 end
-    jeu
 
-puts "bravo tu as gagné"
+# joue !
+jeu
+
+puts "Bravo tu as gagné !\n\n\n"
+
+def average_finish_time
+    # faire tourner le jeu 100 fois
+    tours = Array.new
+    (1..100).each do |i|
+      tours.push(rand(10..100)) # juste un array au hasard : devrait reprendre le nombre de tours de jeu
+    end
+    puts "100 chiffres entre 10 et 100 au hasard"
+    puts tours.inspect
+    # faire la moyenne
+    puts "Moyenne : #{tours.inject(0.0) { |sum, el| sum + el } / tours.size}"
+end
+  
+average_finish_time
