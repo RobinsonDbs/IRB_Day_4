6551b8f2a8230474cc5f2e2830f88273652a78ec
Olivier <olivier@ruechappe.fr>
2019-01-10 22:49:09 +0100

    modifs esthétiques sur messages

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -6,82 +6,76 @@
 
 #Half-Pyramid
 def half_pyramid
-puts "Bonjour, donne un chiffre entre 1 et 25 :"
-print "> "
-chiffre = gets.chomp.to_i
-print chiffre
-if chiffre < 1 || chiffre > 25
-    puts " : Non ! Un chiffre entre 1 et 25 !"
-    exit
-else
-    puts " ? OK. Voici ta pyramide :"
-end
+    print "\n\n\nBonjour, donne un chiffre entre 1 et 25 : "
+    chiffre = gets.chomp.to_i
+    if chiffre < 1 || chiffre > 25
+        puts " : Non ! Un chiffre entre 1 et 25 !"
+        exit
+    else
+        puts "OK. Voici ta pyramide :\n\n"
+    end
 
-i = 1
-while i <= chiffre
-    print " " * (chiffre - i)
-    puts "#" * i
-    i += 1
+    i = 1
+    while i <= chiffre
+        print " " * (chiffre - i)
+        puts "#" * i
+        i += 1
+    end
 end
-end
-
-half_pyramid
 
 #Full_Pyramid
 def full_pyramid
-    puts "Bonjour, donne un chiffre entre 1 et 25 :"
-    print "> "
-chiffre = gets.chomp.to_i
-    print chiffre
 
+    print "\n\n\nBonjour, donne un chiffre entre 1 et 25 > "
+    chiffre = gets.chomp.to_i
 
-if chiffre < 1 || chiffre > 25
-    puts " : Non ! Un chiffre entre 1 et 25 !"
-    exit
-else
-    puts " ? OK. Voici ta pyramide :"
-end
+    if chiffre < 1 || chiffre > 25
+        puts " : Non ! Un chiffre entre 1 et 25 !"
+        exit
+    else
+        puts "OK. Voici ta pyramide :\n\n"
+    end
     i = 1
-while i <= chiffre
-    print " " * (chiffre - i)
-    print "#" * i
-    puts "#" * (i-1)
-    i += 1
+    while i <= chiffre
+        print " " * (chiffre - i)
+        print "#" * i
+        puts "#" * (i-1)
+        i += 1
+    end
 end
-end
-
-full_pyramid
 
 #WTF_Pyramid
 def wtf_pyramid
-    puts "Bonjour, donne un chiffre impair entre 1 et 25 :"
-    print "> "
-chiffre = gets.chomp.to_i
-    print chiffre
+    print "\n\n\nBonjour, donne un chiffre impair entre 1 et 25 : "
+    chiffre = gets.chomp.to_i
 
+    if chiffre < 1 || chiffre > 25 || chiffre.even?
+        puts " : Non ! Un chiffre **impair** entre 1 et 25 !"
+        exit
+    else
+        puts "OK. Voici ta pyramide :\n\n"
+    end
 
-if chiffre < 1 || chiffre > 25 || chiffre.even?
-    puts " : Non ! Un chiffre impair entre 1 et 25 !"
-    exit
-
-else
-    puts " ? OK. Voici ta pyramide :"
-end
     i = 1
-while i <= chiffre/2+1
-    print " " * (chiffre - i)
-    print "#" * i
-    puts "#" * (i-1)
-    i += 1
+    while i <= chiffre/2+1
+        print " " * (chiffre - i)
+        print "#" * i
+        puts "#" * (i-1)
+        i += 1
+    end
+
+    until i == 0
+        print " " * (chiffre - i)
+        print "#" * i
+        puts "#" * (i-1)
+        i -= 1
+    end
 end
 
-until i == 0
-    print " " * (chiffre - i)
-    print "#" * i
-    puts "#" * (i-1)
-    i -= 1
-
-end
+def perform
+    half_pyramid
+    full_pyramid
+    wtf_pyramid
 end
 
-wtf_pyramid
+perform
